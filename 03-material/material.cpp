c91791d9fb581e87379aa7e15810fba0c37f832f
Leszek Godlewski <github@inequation.org>
2013-10-13 21:02:35 +0200

    updates to the material sample

--- 03-material/material.cpp
+++ 03-material/material.cpp
@@ -1,6 +1,4 @@
-#include <cstdio>
 #include <vector>
-
 using namespace std;
 
 #if _WIN32 || _WIN64
@@ -13,25 +11,37 @@ using namespace std;
 	#define DebugBreak()
 #endif
 
+// class to emulate a material for a 3D surface
 class Material
 {
 public:
+	// an index uniquely identifies a material instance
 	Material() : Index(Counter++) {}
 	inline int GetIndex() const {return Index;}
 
 private:
 	int			Index;
+
+	// used to assign consecutive indices to new material instances
 	static int	Counter;
 };
 
 int Material::Counter = 0;
 
+// class to emulate a game object/actor/agent
 class Object
 {
 public:
-	Object(int InIndex, Material *InMaterial) : Index(InIndex), ObjMaterial(InMaterial) {}
+	// an index uniquely identifies an object
+	Object(int InIndex, Material *InMaterial) : Index(InIndex), ObjMaterial(nullptr)
+	{
+		// use the setter on purpose to use the "game logic"
+		SetMaterial(InMaterial);
+	}
+
 	inline int GetIndex() const {return Index;}
 
+	// setter for material reference - emulates the presence of additional game logic
 	inline void SetMaterial(Material *InMaterial)
 	{
 		if (ObjMaterial == InMaterial)
@@ -51,19 +61,27 @@ int main(int argc, char *argv[])
 	static const size_t ObjectCount = 100000;
 	vector<Object *> Objects(ObjectCount);
 	for (size_t i = 0; i < Objects.size(); ++i)
+	{
+		// create the object
 		Objects[i] = new Object(i, new Material());
+		// this is the culprit we're looking for: for every 30 objects, have 10 have their material reset
+		if ((i / 10) % 3 == 0)
+			Objects[i]->SetMaterial(nullptr);
+	}
 
+	// emulate main game loop
 	while (true)
 	{
 		for (auto it = Objects.begin(); it != Objects.end(); it++)
 		{
+			// emulate some gameplay-related state changes, reflected in changing the material
 			const int Chance = rand() % 100;
-			if (Chance < 2)
-				(*it)->SetMaterial(nullptr);
-			else if (Chance < 40)
+			if (Chance < 40)
 				(*it)->SetMaterial(new Material);
 		}
 	}
+
+	// no need to clean up since the loop above never exits and this program is just for educational purposes :)
 
 	return 0;
 }
